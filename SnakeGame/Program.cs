f002e6351fbcf7d4fddde7e73bd5e0ca7856f406
Aamir-shakil <aamirshakil21@gmail.com>
2025-07-14 12:04:18 +0100

    Feat: Create random position of apple

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -3,13 +3,20 @@
 //Dimensions of play area 
 Coord gridDimensions = new Coord(50, 20);
 Coord snakeStartPosition = new Coord(10, 1);
+Random random = new Random();
+Coord applePosition = new Coord(random.Next(1, gridDimensions.X - 1), random.Next(1, gridDimensions.Y - 1));//Random position for appple within dimensions of play area 
 
 for (int y = 0; y < gridDimensions.Y; y++) {
     for (int x = 0; x < gridDimensions.X; x++)
     {
         Coord currentCord = new Coord(x, y);
-        if (snakeStartPosition.Equals(currentCord)) {
+        if (snakeStartPosition.Equals(currentCord))
+        {
             Console.Write("■");
+        }
+        else if (applePosition.Equals(currentCord))
+        {
+            Console.Write("@");
         }
 
         else if (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
