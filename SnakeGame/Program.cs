3862884295904d9afbf3ed0628e382c07b9d39c2
Aamir-shakil <aamirshakil21@gmail.com>
2025-07-14 11:55:02 +0100

    Fix: "Writeline" causing line break diorientating the wall fixed changes to "Write".

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -9,7 +9,7 @@ for (int y = 0; y < gridDimensions.Y; y++) {
     {
         Coord currentCord = new Coord(x, y);
         if (snakeStartPosition.Equals(currentCord)) {
-            Console.WriteLine("■");
+            Console.Write("■");
         }
 
         else if (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
