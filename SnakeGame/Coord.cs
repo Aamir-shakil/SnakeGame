af5c668965a5fd2e6d36b841938b831b25094579
Aamir-shakil <aamirshakil21@gmail.com>
2025-07-14 10:59:32 +0100

    Feat: Add Coord Class

--- SnakeGame/Coord.cs
+++ SnakeGame/Coord.cs
@@ -8,5 +8,28 @@ namespace SnakeGame
 {
     internal class Coord
     {
+        private int x;
+        private int y;
+
+        // Public read-only property to access the x coordinate
+        public int X
+        {
+            get { return x; }
+        }
+
+        // Public read-only property to access the y coordinate
+        public int Y
+        {
+            get { return y; }
+        }
+
+        public Coord(int x, int y) {
+            this.x = x;
+            this.y = y; 
+        }
+
+        
     }
-}
+    }
+
+
